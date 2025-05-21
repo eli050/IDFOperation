d27a78c6dd9b60bf7caa8124989d120764674438
eli <lykkh78@gmail.com>
2025-05-21 12:55:41 +0300

    changing the IDF & AttackOption classes

--- IDF.cs
+++ IDF.cs
@@ -6,29 +6,32 @@ namespace IDFOperation.IDFClasses
     {
         private string dateOfEstablishment;
         private string currentCommander;
-        // List of attack options
-        public IDF(string DateOfEstablishment, string CurrentCommander)
+        private List<AttackOption> attacks;
+        public IDF(string DateOfEstablishment, string CurrentCommander 
+            , List<AttackOption> Attacks)
         {
             dateOfEstablishment = DateOfEstablishment;
             currentCommander = CurrentCommander;
-            // List of attack options
+            attacks = Attacks;
         }
 
     }
-    abstract class AttackOption
+   public abstract class AttackOption
     {
         private string uniqueName;
         private int ammunitionCapacity;
         private long fuelSupply;
         private List<string> effectiveAgainst;
+        private string bombType;
         public AttackOption(string UniqeName ,
             int AmmunitionCapacity, long FuelSupply,
-            List<string> EffectiveAgainst)
+            List<string> EffectiveAgainst, string BombType)
         {
             uniqueName = UniqeName;
             ammunitionCapacity = AmmunitionCapacity;
             fuelSupply = FuelSupply;
             effectiveAgainst = EffectiveAgainst;
+            bombType = BombType;
         }
     }
 }
