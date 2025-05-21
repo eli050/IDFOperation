6bf24d99dd008e157af21d509d87a303efd8d1dd
asher <aw0534145493@gmail.com>
2025-05-21 16:56:02 +0300

    adding Hamas Class and Terrorist Class

--- Aman.cs
+++ Aman.cs
@@ -1,24 +1,24 @@
 ﻿using System;
+using IDFOperation.HamasClasses;
 
 namespace IDFOperation.AmanClasses
 {
     // intelligance class - Intelligence Management
     public class Aman
     {
-        List < IntelligenceMessage > IntelligenceMessages = new List < IntelligenceMessage >();
+        private List < IntelligenceMessage > IntelligenceMessages = new List < IntelligenceMessage >();
 
         //Method for adding intelligence information to a list
     }
     //Defines intelligence information
     public class IntelligenceMessage
     {
-        private string terrorist;
+        private Terrorist terrorist;
         private string lastLocation;
         private string timeStemp;
-        //It requires defining a special class for the terrorist and changing the input.
-        public IntelligenceMessage(string Terrorist, string LastLocation, string TimeStemp)
+        public IntelligenceMessage( Terrorist dangourTerroist, string LastLocation, string TimeStemp)
         {
-            terrorist = Terrorist;
+            terrorist = dangourTerroist;
             lastLocation = LastLocation;
             timeStemp = TimeStemp;
         }
