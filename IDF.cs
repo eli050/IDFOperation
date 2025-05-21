d13b6a6862dac28ab34fe2d4cf4b9e633bfc66b7
eli <lykkh78@gmail.com>
2025-05-21 11:17:54 +0300

    Skeleton of a platoon named the IDF

---
+++ IDF.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IDFOperation.IDFClasses
+{
+    public class IDF
+    {
+        private string dateOfEstablishment;
+        private string currentCommander;
+        // List of attack options
+        public IDF(string DateOfEstablishment, string CurrentCommander)
+        {
+            dateOfEstablishment = DateOfEstablishment;
+            currentCommander = CurrentCommander;
+            // List of attack options
+        }
+
+    }
+}
