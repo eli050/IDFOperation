f6f2099b600541ec89a62a5774bc079136a2f811
eli <lykkh78@gmail.com>
2025-05-22 15:46:33 +0300

    adding skeleton of menu func

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using IDFOperation.AmanClasses;
+using IDFOperation.IDFClasses;
 namespace IDFOperation
 {
     class Program
@@ -9,6 +11,33 @@ namespace IDFOperation
         }
         static void Menu()
         {
+            bool NotExit = true;
+            Aman aman = new Aman();
+            IDF idf = new IDF();
+            do
+            {
+                PrintMenu();
+                string? choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "a":
+                        Console.WriteLine(aman.IntelligenceAnalysis());
+                        break;
+                    case "b":
+                        Console.WriteLine(idf.GetStrikeAndArsenal());
+                        break;
+                    case "c":
+                        Console.WriteLine(aman.MostDangoursTerrorist());
+                        break;
+                    case "d":
+                        break;
+                    case "e":
+                        NotExit = false;
+                        break;
+
+                }
+
+            } while (NotExit);
 
         }
         static void PrintMenu()
@@ -17,7 +46,8 @@ namespace IDFOperation
                 "\na. Intelligence Analysis, identifying the terrorist with the most intelligence reports" +
                 "\nb. Strike Availability, displaying all currently available strike units and their capacity" +
                 "\nc. Target Prioritization, determining the most dangerous terrorist based on a quality rating" +
-                "\nd. Strike Execution, selecting an appropriate strike unit based on the terrorist's location");
+                "\nd. Strike Execution, selecting an appropriate strike unit based on the terrorist's location\n" +
+                "e. Exit");
         }
     }
 }
