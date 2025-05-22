09e21b811350a6e1300e64bcad7abbc61a3ebdc4
eli <lykkh78@gmail.com>
2025-05-22 14:33:47 +0300

    adding the Print menu func

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+namespace IDFOperation
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+        static void Menu()
+        {
+
+        }
+        static void PrintMenu()
+        {
+            Console.WriteLine("Here is a military management menu. Please enter your selection:" +
+                "\na. Intelligence Analysis, identifying the terrorist with the most intelligence reports" +
+                "\nb. Strike Availability, displaying all currently available strike units and their capacity" +
+                "\nc. Target Prioritization, determining the most dangerous terrorist based on a quality rating" +
+                "\nd. Strike Execution, selecting an appropriate strike unit based on the terrorist's location");
+        }
+    }
+}
